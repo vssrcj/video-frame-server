3e2c1a3ee42f1ed83431e21c1813a94241c6ab25
CJ <vssrcj@gmail.com>
2018-02-22 17:47:00 +0200

    changes

--- app.js
+++ app.js
@@ -6,36 +6,36 @@ var fs = require('fs');
 var dir = path.join(__dirname, 'public');
 
 var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript'
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript'
 };
 
 let timestamp = 0;
 
 app.get('/image', function (req, res) {
-   // console.log(req.path, req.path.replace(/\/$/, '/index.html'));
-   var image = timestamp % 5;
-    var file = path.join(dir, '/images/' + image + '.jpg');
-   //  var file = 'background.jpg'
-    if (file.indexOf(dir + path.sep) !== 0) {
-        return res.status(403).end('Forbidden');
-    }
-    var type = mime[path.extname(file).slice(1)] || 'text/plain';
-    var s = fs.createReadStream(file);
-    s.on('open', function () {
-        res.set('Content-Type', type);
-        s.pipe(res);
-    });
-    s.on('error', function () {
-        res.set('Content-Type', 'text/plain');
-        res.status(404).end('Not found');
-    });
+   var s = "0000" + timestamp.toString();
+
+  var file = path.join(dir, '/images/video ' + s.substr(s.length - 4) + '.jpg');
+
+  if (file.indexOf(dir + path.sep) !== 0) {
+      return res.status(403).end('Forbidden');
+  }
+  var type = mime[path.extname(file).slice(1)] || 'text/plain';
+  var s = fs.createReadStream(file);
+  s.on('open', function () {
+      res.set('Content-Type', type);
+      s.pipe(res);
+  });
+  s.on('error', function () {
+      res.set('Content-Type', 'text/plain');
+      res.status(404).end('Not found');
+  });
 });
 
 app.get('/', function (req, res) {
@@ -48,7 +48,7 @@ app.get('/timestamp', function (req, res) {
 
 app.listen(3000, function () {
    setInterval(function() {
-      timestamp += 1;
-   }, 100);
-    console.log('Listening on http://localhost:3000/');
+      timestamp = timestamp >= 1257 ? 1 : (timestamp + 1);
+   }, 40);
+  console.log('Listening on http://localhost:3000/');
 });
